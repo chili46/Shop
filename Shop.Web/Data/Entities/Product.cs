01f9986c408a51118a9ab77308671487c34ed72c
Luis Silva <luisfsilva4664@gmail.com>
2019-02-24 16:39:17 -0500

    Adding Other Methods To Generic Repository (OrderBy)

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -32,5 +32,18 @@ namespace Shop.Web.Data.Entities
 
         public User User { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(this.ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://shopchili46.azurewebsites.net{this.ImageUrl.Substring(1)}";
+            }
+        }
+
     }
 }
